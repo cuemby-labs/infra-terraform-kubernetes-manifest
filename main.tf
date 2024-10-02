b5a33fdf9ec02d7806afbb0cc22c0971e13027e4
Manuel Nuila <manuel.nuila@cuemby.com>
2024-10-02 17:20:38 -0600

    test document value

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ data "kubectl_file_documents" "manifest_url" {
 }
 
 resource "kubectl_manifest" "install_manifest_url" {
-  for_each  = { for key, doc in data.kubectl_file_documents : key => doc.manifests }
+  for_each  = { for key, doc in data.kubectl_file_documents : key => doc.documents }
   yaml_body = each.value
 }
