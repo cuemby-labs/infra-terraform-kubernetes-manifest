5b4d86f91809bb374bf33bb582fe207bfff21824
Manuel Nuila <manuel.nuila@cuemby.com>
2024-10-02 17:32:59 -0600

    add test values

--- main.tf
+++ main.tf
@@ -7,12 +7,13 @@ data "http" "manifest_url" {
   url      = each.value
 }
 
-data "kubectl_file_documents" "manifest_url" {
+data "kubectl_file_documents" "manifest_files" {
   for_each = data.http.manifest_url
   content  = each.value.response_body
 }
 
-resource "kubectl_manifest" "install_manifest_url" {
-  for_each  = { for key, doc in data.kubectl_file_documents : key => doc.documents }
-  yaml_body = each.value
+resource "kubectl_manifest" "install_manifest_files" {
+  for_each  = { for key, doc in data.kubectl_file_documents.manifest_files : key => doc.documents }
+  # yaml_body = each.value
+  yaml_body = join("\n---\n", each.value)
 }
